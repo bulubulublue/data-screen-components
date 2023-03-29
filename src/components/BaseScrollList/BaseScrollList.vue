161f48077704a35719fadedc9ab243c3f5a5a09e
ChenCheng-0715 <bendelayoc@gmail.com>
2023-03-29 17:07:10 +0800

    feat: 添加滚动列表

--- src/components/BaseScrollList/BaseScrollList.vue
+++ src/components/BaseScrollList/BaseScrollList.vue
@@ -16,22 +16,27 @@
         v-html="item"
       ></div>
     </div>
-    <div
-      class="base-scroll-list-rows"
-      v-for="(row, i) in rowData"
-      :key="i"
-      :style="{
-        height: `${rowHeights[i]}px`,
-        backgroundColor: `${i % 2 === 0 ? rowBg[1] : rowBg[0]}`,
-      }"
-    >
+    <div class="rows-wrapper" :style="{ height: `${rowsContainerHeight}px` }">
+      <!-- {{ currentRowData }} -->
       <div
-        class="row-item"
-        :style="{ width: `${columnWidths[i]}px`, ...rowStyle[i] }"
-        v-for="(item, i) in row"
-        :key="item + i"
-        v-html="item"
-      ></div>
+        class="base-scroll-list-rows"
+        v-for="(row, index) in currentRowData"
+        :key="row.rowIndex"
+        :style="{
+          height: `${rowHeights[index]}px`,
+          lineHeight: `${rowHeights[index]}px`,
+          backgroundColor: `${row.rowIndex % 2 === 0 ? rowBg[1] : rowBg[0]}`,
+        }"
+      >
+        <!-- 在这里添加base-scroll-list-text就能解决滚动时有残留的问题 -->
+        <div
+          class="row-item base-scroll-list-text"
+          :style="{ width: `${columnWidths[i]}px`, ...rowStyle[i] }"
+          v-for="(item, i) in row.data"
+          :key="item + i"
+          v-html="item"
+        ></div>
+      </div>
     </div>
   </div>
 </template>
@@ -45,8 +50,14 @@ export default {
   name: 'BaseScrollList',
   props: {
     header: Array,
-    headerStyle: Array, //控制header每一列的样式
-    rowStyle: Array, // 控制内容每一列样式
+    headerStyle: {
+      type: Array,
+      default: [],
+    }, //控制header每一列的样式
+    rowStyle: {
+      type: Array,
+      default: [],
+    }, // 控制内容每一列样式
     headerBackground: {
       type: String,
       default: 'rgb(90,90,90)',
@@ -85,6 +96,11 @@ export default {
     const id = `base-scroll-list-${uuid}`
     const { height, width } = useSize(id) //获取容器宽高
     const rowData = ref([])
+    const currentRowData = ref([])
+    const currentIndex = ref(0)
+    const duration = ref(2000) //动画间隔时间
+    const rowsContainerHeight = ref(0)
+    const rowAvgHeight = ref(0)
 
     const handleHeader = () => {
       const headerClone = cloneDeep(props.header)
@@ -125,8 +141,24 @@ export default {
       if (rowsClone.length === 0) {
         return
       }
+
+      // if (
+      //   rowsClone.length >= props.pageSize &&
+      //   rowsClone.length <= props.pageSize * 2
+      // ) {
+      //   rowsClone = [...rowsClone, ...rowsClone]
+      // }
+
       if (props.headerIndex) {
-        rowsClone = rowsClone.map((row, index) => [index + 1, ...row])
+        rowsClone = rowsClone.map((row, index) => ({
+          data: [index + 1, ...row],
+          rowIndex: index, // 因为数组顺序有改变，所以不能用array的index作为key
+        }))
+      } else {
+        rowsClone = rowsClone.map((row, index) => ({
+          data: row,
+          rowIndex: index, // 因为数组顺序有改变，所以不能用array的index作为key
+        }))
       }
 
       rowData.value = rowsClone
@@ -136,13 +168,48 @@ export default {
     const handleRowHeight = () => {
       const headerHeight = props.headerHeight
       const unusedHeight = height.value - headerHeight
+      rowsContainerHeight.value = unusedHeight
       //每页显示行数，如果pageSIze>总条数，则以总条数为rowNum
       const rowNum =
         props.pageSize > rowData.value.length
           ? rowData.value.length
           : props.pageSize
       const avgHeight = unusedHeight / rowNum
-      rowHeights.value = new Array(rowData.value.length).fill(avgHeight)
+      rowAvgHeight.value = avgHeight
+      rowHeights.value = new Array(rowNum).fill(avgHeight)
+    }
+
+    // 滚动动画
+    const startAnimation = async () => {
+      const dataRaw = cloneDeep(rowData.value)
+
+      const index = currentIndex.value
+      const rowNum = props.pageSize
+      const totalLength = dataRaw.length
+      if (totalLength < rowNum) return // 如果页面条数>总条数，则不做动画
+      // 进行表格数据头尾连接：[a,b,c,d,e,f] index=1,即指向b, 则数据处理为[b,c,d,e,f,a]
+      const dataNew = [...dataRaw.slice(index), ...dataRaw.slice(0, index)]
+      console.log(dataNew)
+      currentRowData.value = dataNew
+
+      //调整高度，现将所有行的高度还原
+      rowHeights.value = new Array(dataRaw.length).fill(rowAvgHeight.value)
+
+      await new Promise((resolve) => setTimeout(resolve, 300))
+
+      //将当前滚动行的高度调整为0
+      rowHeights.value.splice(0, 1, 0)
+
+      if (currentIndex.value >= dataRaw.length - 1) {
+        currentIndex.value = 0
+      } else {
+        currentIndex.value += 1
+      }
+
+      // 用同步的方法实现休眠操作
+      await new Promise((resolve) => setTimeout(resolve, duration.value - 300))
+
+      startAnimation()
     }
 
     onMounted(() => {
@@ -158,31 +225,44 @@ export default {
       columnWidths,
       rowData,
       rowHeights,
+      currentRowData,
+      rowsContainerHeight,
     }
   },
 }
 </script>
 <style lang="scss" scoped>
 .base-scroll-list {
-  width: 100%;
-  height: 100%;
-
   &-header {
     display: flex;
-    font-size: 15px;
+    font-size: 24px;
+
+    .header-item {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
   }
 
   &-text {
     padding: 0 10px;
     box-sizing: border-box;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
   }
 
   &-rows {
     display: flex;
     align-items: center;
+    transition: all 0.3s linear;
     .row-item {
       font-size: 28px;
     }
+  }
+
+  .rows-wrapper {
+    overflow: hidden;
   }
 }
 </style>
